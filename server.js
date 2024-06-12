fda3a3805e1eb1a835a2225ebfe2b71f175fb69a
Darren <manchingdarrensiu@Darrens-MBP.attlocal.net>
2024-06-12 11:48:06 -0700

    modified the server

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post('/solve-cube', (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
